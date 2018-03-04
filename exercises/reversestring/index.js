847300f7929a098301bcc08324b93097cd7b8973
n-kimberly <kimberlyvnguyen@gmail.com>
2018-03-04 13:26:49 -0800

    Pass reverseString.js

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// // Initial attempt
+// function reverse(str) {
+//     var output = '';
+//     for (var i = str.length-1; i >= 0; i--) {
+//         output = output + str[i];
+//     }
+//     return output;
+// }
 
+// // Using es15 syntax
+// function reverse(str) {
+//     let reversed = '';
+//     for (let character of str) {
+//         reversed = character + reversed
+//     }
+//     return reversed;
+// }
+
+// // Using Array.prototype.reverse()
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// // Not using Array.prototype.reverse()
+// function reverse(str) {
+//     return str.split('').reduce((output, i) => {
+//         return i + output 
+//     }, '');
+// }
+
+//Condensed
+function reverse(str) {
+    return str.split('').reduce((output, i) =>  i + output, '');
+}
 module.exports = reverse;
