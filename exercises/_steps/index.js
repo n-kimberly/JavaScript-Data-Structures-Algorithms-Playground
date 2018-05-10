c87f606a6e3fb905be377dd837fd853a2a656f03
n-kimberly <kimberlyvnguyen@gmail.com>
2018-05-10 13:50:19 -0700

    Develop solution for steps

---
+++ exercises/_steps/index.js
@@ -0,0 +1,30 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+function steps(n) {
+    let pound = '';
+    let space = ' '.repeat(n);
+    for (let i=0; i<n; i++) {
+        pound += '#';
+        space = space.slice(0, -1);
+        console.log(pound+space);
+    }
+}
+
+module.exports = steps;
